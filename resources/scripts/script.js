6e6cffd7c4c623e1ae0267df11a2638acde29d7b
Arei1126 <ducati5334@gmail.com>
2025-08-22 18:07:41 +0900

    create html mock

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,4 +1,24 @@
 `use strict`
+data = {
+    "result": [0,0,0]
+}
+
+
 window.addEventListener("load", async ()=>{
-    alert("どうだ？");
+    const Signal = document.createElement("div");
+
+    Signal.addEventListener("choiced", (ev) => {
+        print(ev.detail);
+        alert(ev.detail);
+    });
+
+
+    const choices = document.querySelectorAll(".choice");
+    for (const choice of choices){
+        choice.addEventListener("pointerup", ()=>{
+            const ev = new CustomEvent("choiced", {
+                detail: choice.id 
+            });
+        })
+    }
 });
